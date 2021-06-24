9c27ff26fa6f9465159df6cc8728ef51f0a986b5
Prahlad Mishra <prahlad.mishra@abbott.com>
2021-06-24 13:21:36 +0100

    Routing and Middleware

--- services/SpeakerService.js
+++ services/SpeakerService.js
@@ -25,9 +25,10 @@ class SpeakerService {
     const data = await this.getData();
 
     // We are using map() to transform the array we get into another one
-    return data.map(speaker => {
-      return { name: speaker.name, shortname: speaker.shortname };
-    });
+    return data.map(speaker => ({
+      name: speaker.name,
+      shortname: speaker.shortname
+    }));
   }
 
   /**
@@ -54,9 +55,7 @@ class SpeakerService {
    */
   async getArtworkForSpeaker(shortname) {
     const data = await this.getData();
-    const speaker = data.find(elm => {
-      return elm.shortname === shortname;
-    });
+    const speaker = data.find(elm => elm.shortname === shortname);
     if (!speaker || !speaker.artwork) return null;
     return speaker.artwork;
   }
@@ -67,9 +66,7 @@ class SpeakerService {
    */
   async getSpeaker(shortname) {
     const data = await this.getData();
-    const speaker = data.find(elm => {
-      return elm.shortname === shortname;
-    });
+    const speaker = data.find(elm => elm.shortname === shortname);
     if (!speaker) return null;
     return {
       title: speaker.title,
@@ -84,13 +81,11 @@ class SpeakerService {
    */
   async getListShort() {
     const data = await this.getData();
-    return data.map(speaker => {
-      return {
-        name: speaker.name,
-        shortname: speaker.shortname,
-        title: speaker.title
-      };
-    });
+    return data.map(speaker => ({
+      name: speaker.name,
+      shortname: speaker.shortname,
+      title: speaker.title
+    }));
   }
 
   /**
@@ -98,14 +93,12 @@ class SpeakerService {
    */
   async getList() {
     const data = await this.getData();
-    return data.map(speaker => {
-      return {
-        name: speaker.name,
-        shortname: speaker.shortname,
-        title: speaker.title,
-        summary: speaker.summary
-      };
-    });
+    return data.map(speaker => ({
+      name: speaker.name,
+      shortname: speaker.shortname,
+      title: speaker.title,
+      summary: speaker.summary
+    }));
   }
 
   /**
